709a9efb7765e96e6e26130516f9983106f97d90
Sunitha Malipeddi <sunitha_malipeddi@yahoo.com>
2021-11-01 11:12:12 +0000

    Exercise-1-Contolled-Components

--- src/App.js
+++ src/App.js
@@ -3,26 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  constructor(props) {
-   super(props)
-   
-   
-    
-    
-  }
-   state = {
-    query: ''
-  }
+  state = {
+    value: '',
+  };
 
-    updateQuery = (query) => {
-    this.setState(() => ({
-      query: query.trim()
-    }))
-    
-  }
-  
+  handleChange = event => {
+    this.setState({ value: event.target.value });
+  };
+
   render() {
-    const { query } =  this.state
     return (
       <div className="App">
         <header className="App-header">
@@ -30,12 +19,16 @@ class App extends Component {
           <h1 className="App-title">ReactND - Coding Practice</h1>
         </header>
         <div className="container">
-          <input type="text" placeholder="Say Something"  value={query}
-          onChange={(event) => this.updateQuery(event.target.value)}/>
+          <input
+            type="text"
+            placeholder="Say Something"
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
           <p className="echo">Echo:</p>
-          <p>{query}</p>
-        </div>
+          <p>{this.state.value}</p> 
       </div>
+     </div>
     );
   }
 }
